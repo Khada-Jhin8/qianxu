7de3e1ca9f3f96f957d31e86e735b40f60a5163c
zhg <570296368@qq.com>
2025-08-09 14:02:30 +0800

    添加验证码

--- node/app.js
+++ node/app.js
@@ -2,23 +2,60 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const cors = require('cors');
+const session = require('express-session');
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const YOUR_CODE = '3f969674-2e25-46ee-8535-b25f661be06a'; // 替换为您实际的唯一Code
 const API_URL = `http://47.100.162.240:12121/api/notify/${YOUR_CODE}`; // 替换为您实际的API URL
 
+
+const svgCaptcha = require('svg-captcha');
+
+
+// Middleware
+app.use(session({
+    secret: 'your-secret-key', // 替换为您的秘密密钥
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } // 在HTTP使用时设置为false，若使用HTTPS则需要设置为true
+}));
 // Middleware
 app.use(cors()); // 允许跨域请求
 app.use(bodyParser.json()); // 解析JSON请求体
 
+// GET /api/captcha 生成验证码
+app.get('/api/captcha', (req, res) => {
+
+    const captcha = svgCaptcha.create({
+        size: 4,
+        noise: 2,
+        color: true,
+        background: '#cc9966'
+    });
+    req.session = req.session || {};
+    req.session.captcha = captcha.text.toLowerCase();
+    res.type('svg');
+    res.status(200).send(captcha.data);
+   
+});
+
 // POST /api/contact
 app.post('/api/contact', async (req, res) => {
-    const { name, email, phone, message } = req.body;
+    const { name, email, phone, message, captcha } = req.body;
+ 
 
     // 校验必填字段
-    if (!message) {
+    if (!message || !name || !email || !phone) {
+
         return res.status(400).json({ error: '消息内容是必填的.' });
+    }
+
+    // 校验验证码
+    if (!captcha ) { //简单判断是否存在。无实际意义
+        return res.status(400).json({ error: '验证码错误.' });
     }
 
     // 构建要发送的内容
@@ -54,3 +91,4 @@ app.post('/api/contact', async (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
